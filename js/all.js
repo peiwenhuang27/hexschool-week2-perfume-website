2d43296d12bdc360291c3a39c201c70e1a90ef90
peiwenhuang27 <b07705025@ntu.edu.tw>
2021-07-24 16:56:57 +0800

    product, product-close up

--- js/all.js
+++ js/all.js
@@ -5,4 +5,29 @@ $(document).ready(function() {
 		$('.dropdown').toggleClass('active');
 		$('.header-menu').fadeToggle();
 	});
+
+	var quantitiy=0;
+	$('.quantity-plus').click(function(e){	
+		// Stop acting like a button
+		e.preventDefault();
+		// Get the field name
+		var quantity = parseInt($('#quantity').val());
+		console.log(quantitiy);
+		
+		// If is not undefined
+		$('#quantity').val(quantity + 1);
+		// Increment
+	});
+	$('.quantity-minus').click(function(e){
+		// Stop acting like a button
+		e.preventDefault();
+		// Get the field name
+		var quantity = parseInt($('#quantity').val());
+		console.log(quantitiy);
+		// If is not undefined
+		// Increment
+		if(quantity > 0){
+		$('#quantity').val(quantity - 1);
+		}
+	});
 });
